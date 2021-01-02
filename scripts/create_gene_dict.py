03cc7ca1ea5c41a0ff338e0762a3b5aceb57dfc2
Vivek Modi <modi.vivek2009@gmail.com>
2021-01-02 12:11:19 -0500

    Added values for cases missing uniprot in create_gene_dict

--- scripts/create_gene_dict.py
+++ scripts/create_gene_dict.py
@@ -11,24 +11,24 @@ import pandas as pd
 def gene_dict(pwd,df):
     print('Creating gene dictionary...')
     df_motifs=pd.read_csv(f'{pwd}/motifs_all.tsv',sep=' ')
-    fhandle_newuniprots=open(f'{pwd}/New_uniprots.txt','w')    
-    
-   
-    
+    fhandle_newuniprots=open(f'{pwd}/New_uniprots.txt','w')
+
+
+
     for i in df.index:
         #pdb=df.at[i,'PDBid']
         uniprot=df.at[i,'UniprotID']
         res1=df.at[i,'StrBegin']
         res2=df.at[i,'StrEnd']
         df.at[i,'UniSeq']=''             #initialize this here otherwise in uniprotseq function it gives an error that key does not exist
-        
+
         if df.at[i,'PDBid']=='4OLIA': #Both domains present, use only the first one
-            res2=875               
-            
+            res2=875
+
         if uniprot in ('KS6A1_HUMAN','KS6A2_HUMAN','KS6A3_HUMAN','KS6A4_HUMAN','KS6A5_HUMAN','KS6A6_HUMAN','OBSCN_HUMAN','SPEG_HUMAN','E2AK4_HUMAN',\
                         'JAK1_HUMAN','JAK2_HUMAN','JAK3_HUMAN','TYK2_HUMAN','KS6A3_MOUSE','JAK2_MOUSE','TYK2_MOUSE'):
-           
-            
+
+
                 df.at[i,'Gene']=df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].Gene.to_string(index=False).strip()
                 df.at[i,'Domain']=df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].Domain.to_string(index=False).strip()
                 df.at[i,'Group']=df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].Group.to_string(index=False).strip()
@@ -49,9 +49,9 @@ def gene_dict(pwd,df):
                 df.at[i,'GTKres']=df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].GTK_res.to_string(index=False).strip()
                 df.at[i,'GTKnum']=int(df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].GTK_num)
                 df.at[i,'Hinge1']=int(df_motifs[(df_motifs.Uniprot==uniprot) & (df_motifs.Domain_start<=(res1+res2)/2) & (df_motifs.Domain_end>=(res1+res2)/2)].Hinge1_num)
-                                 
-     
-                
+
+
+
         else:
             try:
                 #print(uniprot,res1,res2)
@@ -78,7 +78,25 @@ def gene_dict(pwd,df):
                 #df.at[i,'LRLres']='X';df.at[i,'LRLnum']=int(0);df.at[i,'DLWres']='X';df.at[i,'DLWnum']=int(0);df.at[i,'ISRres']='X';df.at[i,'ISRnum']=int(0);  these motifs are not included for now
             except:
                 fhandle_newuniprots.write(f"Missing {uniprot} {df.at[i,'PDBid']}\n")
-    fhandle_newuniprots.close()   
+                df.at[i,'Gene']='XXX'
+                df.at[i,'Domain']='XXX'
+                df.at[i,'Group']='XXX'
+                df.at[i,'UniAcc']='XXX'
+                df.at[i,'DomainBegin']=int(999)
+                df.at[i,'DomainEnd']=int(999)
+                df.at[i,'ALKres']='XXX'
+                df.at[i,'ALKnum']=int(999)
+                df.at[i,'RREres']='XXX'
+                df.at[i,'RREnum']=int(999)
+                df.at[i,'HRDres']='XXX'
+                df.at[i,'HRDnum']=int(999)
+                df.at[i,'DFGres']='XXX'
+                df.at[i,'DFGnum']=int(999)
+                df.at[i,'DFG_Aspres']='XXX'
+                df.at[i,'APEres']='XXX'
+                df.at[i,'APEnum']=int(999)
+                df.at[i,'GTKres']='XXX'
+                df.at[i,'GTKnum']=int(999)
+                df.at[i,'Hinge1']=int(999)
+    fhandle_newuniprots.close()
     return(df)
-            
-            
